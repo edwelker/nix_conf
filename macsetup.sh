76dc8870e08cdb9f60f93c5488d9fe72e8a1b98d
Eddie Welker <eddie.welker@gmail.com>
2014-07-12 01:32:22 -0400

    added to macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -15,7 +15,7 @@ brew cask install firefox iterm2 google-chrome rdio alfred vlc macvim flux vienn
 #remember: http://stackoverflow.com/questions/127591/using-caps-lock-as-esc-in-mac-os-x
 
 #install non-GUI's
-brew install git python python3 tmux vim phantomjs ack wget tree node bash-completion ruby irssi tomcat groovy maven gradle scala jq autossh mosh
+brew install git python python3 tmux vim phantomjs ack wget tree node bash-completion ruby irssi tomcat groovy maven gradle scala jq autossh mosh libev
 
 #add system duplicates
 brew tap homebrew/dupes
